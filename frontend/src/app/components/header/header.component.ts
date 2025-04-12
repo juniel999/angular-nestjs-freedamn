b6c4df00919377fee035f4e640527601ef1c9cb8
Juniel Hssn <junielhussain@gmail.com>
2025-04-13 04:22:57 +0800

    implemented user registration feature with signup form and authentication service integration

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
