7bf3449cb7d20df463ee3308f736a75c9dfb07d8
Juniel Hssn <junielhussain@gmail.com>
2025-04-15 15:32:45 +0800

    refactor header component for improved routing and clean up

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { initializeIcons } from './icons';
 import { provideHttpClient, withInterceptors, HttpRequest, HttpHandlerFn } from '@angular/common/http';
 
